d2a08bb0f8c12bd4843434103fd2a1c884ad3426
ZacJoffe <zacharyjoffe@gmail.com>
2020-01-30 21:52:17 -0500

    First implementation of shader from source fn

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,18 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
 use std::os::raw::c_void;
+use std::ffi::{CString, CStr};
+use std::ptr::null;
+use std::ptr::null_mut;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 5);
+
     let window = video_subsystem
         .window("Game", 900, 700)
         .opengl()
@@ -22,6 +30,7 @@ fn main() {
     let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const c_void);
 
     unsafe {
+        gl::Viewport(0, 0, 900, 700);
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
@@ -39,4 +48,43 @@ fn main() {
 
         window.gl_swap_window();
     }
+}
+
+fn shader_from_source(source: &CStr, kind: gl::types::GLuint) -> Result<gl::types::GLuint, String> {
+    let id = unsafe {
+        gl::CreateShader(kind)
+    };
+
+    unsafe {
+        gl::ShaderSource(id, 1, &source.as_ptr(), null());
+        gl::CompileShader(id);
+    }
+
+    let mut success: gl::types::GLint = 1;
+
+    unsafe {
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+    }
+
+    if success == 0 {
+        let mut len: gl::types::GLint = 0;
+        unsafe {
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+        }
+
+        let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
+        buffer.extend([b' '].iter().cycle().take(len as usize));
+
+        let err: CString = unsafe {
+            CString::from_vec_unchecked(buffer)
+        };
+
+        unsafe {
+            gl::GetShaderInfoLog(id, len, null_mut(), err.as_ptr() as *mut gl::types::GLchar);
+        }
+
+        Err(err.to_string_lossy().into_owned())
+    }
+
+    Ok(id)
 }
