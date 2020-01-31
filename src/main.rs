d4bc300765859896fd245344a5cd6a10f2e18d94
ZacJoffe <zacharyjoffe@gmail.com>
2020-01-30 20:37:32 -0500

    Opengl driver code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 extern crate sdl2;
+extern crate gl;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+
+use std::os::raw::c_void;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
         .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const c_void);
+
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
 
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -21,5 +32,11 @@ fn main() {
                 _ => {}
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
