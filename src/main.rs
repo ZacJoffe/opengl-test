1e7cfadd65da43043653543a456274727aa836b5
ZacJoffe <zacharyjoffe@gmail.com>
2020-01-30 20:23:50 -0500

    Sdl2 driver code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'main,
+                _ => {}
+            }
+        }
+    }
 }
