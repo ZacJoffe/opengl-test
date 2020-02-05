40262a86018a95b96e10ad3226ce7066e53b2c16
ZacJoffe <zacharyjoffe@gmail.com>
2020-02-04 22:25:10 -0500

    Draw triangle

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use sdl2::keyboard::Keycode;
 
 use std::os::raw::c_void;
 use std::ffi::CString;
+use std::ptr::null;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -41,6 +42,36 @@ fn main() {
 
     shader_program.set_used();
 
+    let verticies: Vec<f32> = vec![
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0
+    ];
+
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER, (verticies.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, verticies.as_ptr() as *const gl::types::GLvoid, gl::STATIC_DRAW);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    }
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, (3 * std::mem::size_of::<f32>()) as gl::types::GLint, null());
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
+
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -51,6 +82,13 @@ fn main() {
 
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+        }
+
+        shader_program.set_used();
+        unsafe {
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
         window.gl_swap_window();
