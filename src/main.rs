9e283cb6038289d98ee327ba65466145da68fbbf
ZacJoffe <zacharyjoffe@gmail.com>
2020-02-04 21:28:36 -0500

    Added shader and shader linking code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 extern crate sdl2;
 extern crate gl;
 
+pub mod render_gl;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
 use std::os::raw::c_void;
-use std::ffi::{CString, CStr};
-use std::ptr::null;
-use std::ptr::null_mut;
+use std::ffi::CString;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -34,6 +34,13 @@ fn main() {
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
+    let vert_shader = render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap()).unwrap();
+    let frag_shader = render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap()).unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    shader_program.set_used();
+
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -48,103 +55,4 @@ fn main() {
 
         window.gl_swap_window();
     }
-}
-
-struct Shader {
-    id: gl::types::GLuint
-}
-
-impl Shader {
-    fn from_source(source: &CStr, kind: gl::types::GLuint) -> Result<Shader, String> {
-        let id = unsafe {
-            gl::CreateShader(kind)
-        };
-
-        unsafe {
-            gl::ShaderSource(id, 1, &source.as_ptr(), null());
-            gl::CompileShader(id);
-        }
-
-        let mut success: gl::types::GLint = 1;
-
-        unsafe {
-            gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-        }
-
-        if success == 0 {
-            let mut len: gl::types::GLint = 0;
-            unsafe {
-                gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-            }
-
-            let err = create_cstring(len as usize);
-
-            unsafe {
-                gl::GetShaderInfoLog(id, len, null_mut(), err.as_ptr() as *mut gl::types::GLchar);
-            }
-
-            return Err(err.to_string_lossy().into_owned());
-        }
-
-        Ok(Shader{ id })
-    }
-
-    fn from_vert_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::VERTEX_SHADER)
-    }
-
-    fn from_frag_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::FRAGMENT_SHADER)
-    }
-}
-
-impl Drop for Shader {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteShader(self.id);
-        }
-    }
-}
-
-/*
-fn shader_from_source(source: &CStr, kind: gl::types::GLuint) -> Result<gl::types::GLuint, String> {
-    let id = unsafe {
-        gl::CreateShader(kind)
-    };
-
-    unsafe {
-        gl::ShaderSource(id, 1, &source.as_ptr(), null());
-        gl::CompileShader(id);
-    }
-
-    let mut success: gl::types::GLint = 1;
-
-    unsafe {
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-    }
-
-    if success == 0 {
-        let mut len: gl::types::GLint = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-        }
-
-        let err = create_cstring(len as usize);
-
-        unsafe {
-            gl::GetShaderInfoLog(id, len, null_mut(), err.as_ptr() as *mut gl::types::GLchar);
-        }
-
-        return Err(err.to_string_lossy().into_owned());
-    }
-
-    Ok(id)
-}
-*/
-
-fn create_cstring(len: usize) -> CString {
-    let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
-    buffer.extend([b' '].iter().cycle().take(len as usize));
-
-    unsafe { CString::from_vec_unchecked(buffer) }
 }
